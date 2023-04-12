4bc8515418306a8f97a913b8341d0260472e0a0a
akhil <akhileshreddy9181@gmail.com>
2023-04-12 02:44:41 -0400

    Added Exit Status to script

--- ec2/data.tf
+++ ec2/data.tf
@@ -2,6 +2,6 @@ data "aws_caller_identity" "current" {}
 
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "devops-prcatice_with-ansible"
+  name_regex       = "ami-with-ansible"
   owners           = [data.aws_caller_identity.current.account_id]
 }
