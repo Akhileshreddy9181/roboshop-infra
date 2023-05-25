29874047522c97b42370f69927784e8870fe621d
akhil <akhileshreddy9181@gmail.com>
2023-05-25 15:53:17 +0530

    Added Exit Status to script

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "vpc" {
   source = "git::https://github.com/Akhileshreddy9181/tf-module-vpc"
   env = var.env
+  tags = var.tags
 
   for_each = var.vpc
   vpc_cidr = each.value["vpc_cidr"]
---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+tags = {
+  bu_unit_name = "ecommerce"
+  app_name = "roboshop"
+  owner = "ecom"
+  cost_center = "1221"
+
+}
