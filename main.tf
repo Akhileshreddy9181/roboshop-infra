1cbf588848b5818d1fad5485006af29dbc37203e
akhil <akhileshreddy9181@gmail.com>
2023-07-05 12:58:28 -0400

    Added Exit Status to script

--- main.tf
+++ main.tf
@@ -150,6 +150,7 @@ resource "aws_spot_instance_request" "load-runner" {
   ami = data.aws_ami.ami.id
   instance_type = "t3.medium"
   wait_for_fulfillment = true
+  availability_zone = "us-east-1b"
   vpc_security_group_ids = [data.aws_security_group.def_secgrp_id.id]
 
   tags = merge(
