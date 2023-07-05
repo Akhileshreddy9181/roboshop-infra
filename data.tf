00389c99de0434bad4794447228f6c32e303a570
akhil <akhileshreddy9181@gmail.com>
2023-07-05 12:03:31 -0400

    Added Exit Status to script

--- data.tf
+++ data.tf
@@ -13,11 +13,9 @@ data "aws_vpc" "default_vpc" {
 }
 
 data "aws_security_group" "def_secgrp_id" {
-  vpc_id = data.aws_vpc.default_vpc.id
 
   filter {
     name   = "group-name"
     values = ["allow-all"]
   }
-
 }
